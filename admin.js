c7ce90be184c36adee02878bd35f9be297ac3317
Pavan Badakh <pavanbadakh710@gmail.com>
2026-06-11 23:26:35 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -3,7 +3,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebas
 import {
   getFirestore,
   doc,
-  setDoc
+  setDoc,
+  getDoc,
+  deleteDoc
+  
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -19,6 +22,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+async function getFarmerName() {
+
+let farmerNo =
+document.getElementById("farmerNo").value;
+
+if (!farmerNo) return;
+
+const snap = await getDoc(
+doc(db, "farmers", farmerNo)
+);
+
+if (snap.exists()) {
+
+document.getElementById("name").value =
+snap.data().name || "";
+
+}
+}
 async function saveFarmer() {
 
 alert("Save button works");
@@ -51,4 +72,79 @@ await setDoc(
   "Data Saved Successfully";
 }
 
+async function loadRecord() {
+
+let farmerNo =
+document.getElementById("farmerNo").value;
+
+let date =
+document.getElementById("date").value;
+
+let session =
+document.getElementById("session").value;
+
+const ref = doc(
+db,
+"farmers",
+farmerNo,
+"records",
+date + "_" + session
+);
+
+const snap = await getDoc(ref);
+
+if (!snap.exists()) {
+    alert("Record Not Found");
+    return;
+}
+
+const data = snap.data();
+
+document.getElementById("litre").value =
+data.litre;
+
+document.getElementById("fat").value =
+data.fat;
+
+document.getElementById("degree").value =
+data.degree;
+
+alert("Record Loaded");
+}
 window.saveFarmer = saveFarmer;
+async function deleteRecord() {
+
+let farmerNo =
+document.getElementById("farmerNo").value;
+
+let date =
+document.getElementById("date").value;
+
+let session =
+document.getElementById("session").value;
+
+let ok = confirm(
+"Are you sure you want to delete this record?"
+);
+
+if (!ok) return;
+
+await deleteDoc(
+doc(
+db,
+"farmers",
+farmerNo,
+"records",
+date + "_" + session
+)
+);
+
+alert("Record Deleted");
+
+document.getElementById("litre").value = "";
+document.getElementById("fat").value = "";
+document.getElementById("degree").value = "";
+}
+window.loadRecord = loadRecord;
+window.deleteRecord = deleteRecord;
+window.getFarmerName = getFarmerName;
